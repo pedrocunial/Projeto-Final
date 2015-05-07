5d1f58b4ecc4f853a496c7a5681674aad66a78f2
pedrocunial <pedro_cunial@hotmail.com>
2015-05-07 19:21:39 -0300

    oba

--- teste2.py
+++ teste2.py
@@ -20,27 +20,23 @@ dispWidth = 800
 dispHeight = 600
 pixMove = 10
 
+
 UP = 'up'
 DOWN = 'down'
 LEFT = 'left'
 RIGHT = 'right'
 
 def runGame():
-	global imgy
-	global pulos
 
 	imgx = 400
 	imgy = 380
-	direction = RIGHT
 	alive = True
-	pulos = 0
-	STANDING = True
-	JUMPING = False
-	FALL = False
+	pressed_up = False
 	g = 0.5
-	pixFall = 0
-	pixJump = pixMove
 	jump_count = 0
+	pygame.mixer.music.load('fdmusic.mp3')
+	pygame.mixer.music.play(-1)
+	pixFall = 0
 
 	while True:     #main loop
 		delay = 100
@@ -49,83 +45,76 @@ def runGame():
 		
 		while alive == True:
 			for event in pygame.event.get():	#event handling loop
-				pressed_up = False
 				pressed_left = False
 				pressed_right = False
 				pressed_down = False
+				bug = False
 				
 				if event.type == QUIT:
 					pygame.quit()
 					sys.exit()
 
-				if event.type == KEYDOWN:
+				elif event.type == KEYDOWN:
 					if event.key == K_LEFT:
 						pressed_left = True
 					elif event.key == K_RIGHT:
 						pressed_right = True
 					elif event.key == K_DOWN:
 						pressed_down = True
-					elif event.key == K_UP:
+					elif event.key == K_UP and jump_count == 0:
 						pressed_up = True
 						jump_count = 38
+						pixFall = 0
+						pixJump = pixMove
 
-				if jump_count > 0:
-					if pixJump > 0:
-						print(pixJump)
-						pixJump = pixJump - g
-						if pixJump < 0:
-							pixJump = 0
-						imgy -= pixJump
 
-					if pixJump <= 0:
-						pixFall += g
-						imgy += pixFall
-						if imgy >= 380:
-							imgy = 380
-							pixJump = pixMove
-							pixFall = 0
-
-					jump_count -= 1
-
-				#elif pressed_up:
-				#	imgy -= pixMove
-				#elif pressed_down:
-					#imgy += pixMove    #tentando fazer pulos
 				if pressed_left:
 					imgx -= pixMove
 				elif pressed_right:
 					imgx += pixMove
 
+			if jump_count > 0 and imgy <= 380:  #jump
+				if pressed_left:
+					imgx -= pixMove/5  #ITS A FEATURE!!
+				elif pressed_right:
+					imgx += pixMove/5  #IT IS STILL A FEATURE!!
 
-				setDisplay.fill(bg)
-				img = pygame.image.load('kirby.png')
-				fase = pygame.draw.rect(setDisplay, purple, (200, 400, 400, 20))
-				imgWidth = 20
-				imgHeight = 20
-				img = pygame.transform.scale(img, (imgWidth,imgHeight))
-				setDisplay.blit(img,(imgx,imgy))
-				pygame.display.update()
+				if pixJump > 0: #UP WE GO!
+					pixJump = pixJump - g
+					if pixJump < 0: #falls
+						pixJump = 0
+					imgy -= pixJump
 
-				if (imgx < 0 or imgy < 0 or imgx > (dispWidth - imgWidth) or imgy > (dispHeight - imgHeight)):
-					#kills
-					alive = False
-					print('You Died')
-					pygame.quit()
-					sys.exit()
+				elif pixJump <= 0:
+					pixFall += g
+					imgy += pixFall
+					if imgy >= 380:
+						pixJump = pixMove
+						pixFall = 0
+						pressed_up = False
+				if jump_count == 1:
+					imgy = 380
+				jump_count -= 1
 
-#def pulo():
-#	global pulos
-#	global imgy
-#
-#		if pulos < 3:
-#			imgy -= pixMove
-#		elif pulos < 5:
-#			imgy -= pixMove
-#		else:
-#			pulos = 0
+			if imgx < 180 or imgx > 600:
+				pixFall += g
+				imgy += pixFall				
+			
+			setDisplay.fill(bg)
+			img = pygame.image.load('kirby.png')
+			fase = pygame.draw.rect(setDisplay, purple, (200, 400, 400, 20))
+			imgWidth = 20
+			imgHeight = 20
+			img = pygame.transform.scale(img, (imgWidth,imgHeight))
+			setDisplay.blit(img,(imgx,imgy))
+			pygame.display.update()
 
-
-
+			if (imgx < 0 or imgy < 0 or imgx > (dispWidth - imgWidth) or imgy > (dispHeight - imgHeight)):
+				#kills
+				alive = False
+				print('You Died')
+				pygame.quit()
+				sys.exit()
 
 while True:
 	global fpsTime
